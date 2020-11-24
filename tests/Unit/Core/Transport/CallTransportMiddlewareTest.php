376a4292e3ebac50baba76178873055ec8291a53
Nathan Boiron <nathan.boiron@gmail.com>
2020-11-24 23:34:10 +0100

    Add missing tests

--- tests/Unit/Core/Transport/CallTransportMiddlewareTest.php
+++ tests/Unit/Core/Transport/CallTransportMiddlewareTest.php
@@ -3,12 +3,14 @@
 namespace CuyZ\WebZ\Tests\Unit\Core\Transport;
 
 use CuyZ\WebZ\Core\Bus\Pipeline\Next;
+use CuyZ\WebZ\Core\Exception\NoCompatibleAsyncTransportException;
 use CuyZ\WebZ\Core\Exception\NoCompatibleTransportException;
 use CuyZ\WebZ\Core\Result\Result;
 use CuyZ\WebZ\Core\Transport\CallTransportMiddleware;
 use CuyZ\WebZ\Core\Transport\NoTransportException;
 use CuyZ\WebZ\Tests\Fixture\Transport\DummyExceptionTransport;
 use CuyZ\WebZ\Tests\Fixture\Transport\DummyIncompatibleTransport;
+use CuyZ\WebZ\Tests\Fixture\Transport\DummySynchronousIncompatibleTransport;
 use CuyZ\WebZ\Tests\Fixture\Transport\DummyTransport;
 use CuyZ\WebZ\Tests\Fixture\WebService\DummyWebService;
 use Exception;
@@ -27,7 +29,7 @@ class CallTransportMiddlewareTest extends TestCase
         new CallTransportMiddleware([]);
     }
 
-    public function test_throws_when_no_transport_is_found()
+    public function test_throws_when_no_synchronous_transport_is_found()
     {
         $this->expectException(NoCompatibleTransportException::class);
 
@@ -41,9 +43,45 @@ class CallTransportMiddlewareTest extends TestCase
         $middleware->process($webservice, $next);
     }
 
-    public function test_calls_the_correct_transport()
+    public function test_throws_when_no_asynchronous_transport_is_found()
+    {
+        $this->expectException(NoCompatibleAsyncTransportException::class);
+
+        $webservice = new DummyWebService(new stdClass());
+        $webservice->setPayloadGroupHash('foo');
+
+        $middleware = new CallTransportMiddleware([
+            new DummySynchronousIncompatibleTransport(),
+        ]);
+
+        $next = new Next(fn() => null);
+
+        $middleware->process($webservice, $next);
+    }
+
+    public function test_calls_the_correct_synchronous_transport()
     {
         $webservice = new DummyWebService(new stdClass());
+
+        $middleware = new CallTransportMiddleware([
+            new DummyIncompatibleTransport(),
+            new DummyTransport(['foo' => 'bar']),
+        ]);
+
+        $next = new Next(fn() => null);
+
+        /** @var Result $result */
+        $result = $middleware->process($webservice, $next)->wait();
+
+        self::assertInstanceOf(Result::class, $result);
+        self::assertNull($result->exception());
+        self::assertSame(['foo' => 'bar'], $result->data());
+    }
+
+    public function test_calls_the_correct_asynchronous_transport()
+    {
+        $webservice = new DummyWebService(new stdClass());
+        $webservice->setPayloadGroupHash('foo');
 
         $middleware = new CallTransportMiddleware([
             new DummyIncompatibleTransport(),
